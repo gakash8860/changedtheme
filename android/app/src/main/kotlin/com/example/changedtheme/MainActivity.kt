d586c966e11a4d94576d05c1531c94a7da7b810a
Akash <53900437+gakash8860@users.noreply.github.com>
2021-01-12 11:31:13 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/changedtheme/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.changedtheme
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main()=>runApp(MyApp());
+
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _switchValue=true;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: _switchValue?Colors.white:Colors.blue,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Changed Theme'),
+            Switch(value: _switchValue, onChanged: (newValue){
+              setState(() {
+                _switchValue=newValue;
+              });
+            })
+          ],
+        ),
+      ),
+    );
+  }
+}
